714ebf7fc4a480d7a87c42f9f4e45a04f299583b
Arkan wirya Sentika <arkanwiryas@gmail.com>
2025-05-31 00:52:08 +0700

    create function

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 // buat ratelimiter yang cuma allow 100 request per 60 seconds
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(100, "60 s"),
+  limiter: Ratelimit.slidingWindow(20, "60 s"),
 });
 
 export default ratelimit;
